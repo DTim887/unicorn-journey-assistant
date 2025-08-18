a25adbfbeedda77928236582acf46fe7ebd4e729
jinzhihong <jzh887@163.com>
2025-08-18 23:40:22 +0800

    init project

---
+++ src/main/java/com/unicorn/journey/assistant/UnicornJourneyAssistantApplication.java
@@ -0,0 +1,13 @@
+package com.unicorn.journey.assistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnicornJourneyAssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnicornJourneyAssistantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unicorn/journey/assistant/UnicornJourneyAssistantApplicationTests.java
@@ -0,0 +1,14 @@
+package com.unicorn.journey.assistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnicornJourneyAssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+        System.out.println("test");
+	}
+
+}
