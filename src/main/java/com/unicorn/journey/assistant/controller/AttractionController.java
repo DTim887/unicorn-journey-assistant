4157bae6a3c826e909eddc0365cee1f627c6da07
lucky <592934754@qq.com>
2025-08-27 20:34:42 +0800

    *feature add attraction list

---
+++ src/main/java/com/unicorn/journey/assistant/controller/AttractionController.java
@@ -0,0 +1,24 @@
+package com.unicorn.journey.assistant.controller;
+
+import com.unicorn.journey.assistant.controller.vo.Result;
+import com.unicorn.journey.assistant.entity.Attraction;
+import com.unicorn.journey.assistant.service.AttractionService;
+import jakarta.annotation.Resource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class AttractionController {
+
+    @Resource
+    AttractionService attractionService;
+
+    @GetMapping("/attraction/getAll")
+    public Result getAllAttraction() {
+        List<Attraction> allAttractions = attractionService.getAll(Attraction.class);
+        return Result.ok(allAttractions);
+    }
+
+}
