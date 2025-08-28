75145c914146908d3cbb9197716423ddcc9ab0b7
wangj748 <jin.wang2@disney.com>
2025-08-29 02:12:32 +0800

    feat(product): 更新产品信息和数据结构
    - 修改 Product 实体类，增加 attractionId、image 和 quantity 字段
    - 更新 product.json 文件，添加新的产品数据和修改现有产品信息
    - 移除 ProductController 中的 @CrossOrigin 注解

--- src/main/java/com/unicorn/journey/assistant/controller/ProductController.java
+++ src/main/java/com/unicorn/journey/assistant/controller/ProductController.java
@@ -4,7 +4,6 @@ import com.unicorn.journey.assistant.controller.vo.Result;
 import com.unicorn.journey.assistant.entity.Product;
 import com.unicorn.journey.assistant.service.ProductService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,14 +16,12 @@ public class ProductController {
 
     private final ProductService productService;
 
-    @CrossOrigin(origins = "*")
     @GetMapping("/product/{name}")
     public Result getProductByName(@PathVariable("name") String name) {
         Product product = productService.getProductByName(name);
         return Result.ok(product);
     }
 
-    @CrossOrigin(origins = "*")
     @GetMapping("/product/all")
     public Result allProduct() {
         List<Product> allProduct = productService.getAllProduct();
